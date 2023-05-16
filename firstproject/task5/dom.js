41529d0d9d5584a0e87d27e299f6ba788e06dd70
nawab55 <mdnawab3674@gmail.com>
2023-05-16 14:57:45 +0530

    some changes by tagname

---
+++ firstproject/task5/dom.js
@@ -0,0 +1,32 @@
+// // GETELEMENTSBYCLASSNAME
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// // items[1].textContent = 'Hello2';
+// // items[1].style.fontWeight = 'bold';
+// // items[2].style.fontWeight = 'bold';
+
+// items[1].style.color = 'yellow';
+// items[2].style.backgroundColor = 'green';
+
+// for(var i=0; i<items.length; i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+
+// GETELEMENTSBYTAGNAME
+let li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+// items[1].textContent = 'Hello2';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.fontWeight = 'bold';
+
+li[1].style.color = 'yellow';
+li[2].style.backgroundColor = 'green';
+
+for(var i=0; i<li.length; i++){
+    li[i].style.fontWeight = 'bold';
+}
+
+
