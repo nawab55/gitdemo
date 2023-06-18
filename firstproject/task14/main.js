32e489eb941404e091ffb24c41df239f21712cd5
nawab55 <mdnawab3674@gmail.com>
2023-06-18 17:27:52 +0530

    task14

---
+++ firstproject/task14/main.js
@@ -0,0 +1,72 @@
+// Put DOM elements into variables
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+  // Retrieving data
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+  console.log(name);
+//   console.log(email);
+
+  // Storing data
+//   const user = localStorage.setItem('name', name);
+//   const email = localStorage.setItem('email', email);
+    const obj = {
+        name,
+        email
+    }
+    localStorage.setItem(obj.email, JSON.stringify(obj));
+    showUserOnScreen(obj);
+}
+function showUserOnScreen(obj){
+    // Create new list item with user
+    const li = document.createElement('li');
+
+    // Add text node with input values
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+    
+    // create a input button
+  const deleteButton = document.createElement('input');
+  deleteButton.id = 'button';
+  // deleteButton.setAttribute('type', "button");
+  // btn.setAttribute('value', "Delete");
+  deleteButton.type = "button";
+  deleteButton.value = "Delete";
+  deleteButton.onclick = () => {
+    localStorage.removeItem(obj.email);
+    userList.removeChild(li);
+  }
+
+   // create a input editbutton
+   const editButton = document.createElement('input');
+   editButton.id = 'button';
+   // editButton.setAttribute('type', "button");
+   // editButton.setAttribute('value', "Edit");
+   editButton.type = "button";
+   editButton.value = "Edit";
+   editButton.onclick = () => {
+     localStorage.removeItem(obj.email);
+     userList.removeChild(li);
+     document.getElementById('name').value = obj.name;
+     document.getElementById('email').value = obj.email;
+   
+   }
+  li.appendChild(deleteButton);
+  li.appendChild(editButton);
+  // Append to ul
+  userList.appendChild(li);
+
+  
+
+    // Clear fields
+    // nameInput.value = '';
+    // emailInput.value = '';
+}
